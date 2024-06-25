a449e54a60616e33e5ed84375e67666eb8f893d1
AisyA72 <b72-2002-arceus@docomo.ne.jp>
2024-06-25 18:29:55 +0900

    first commit

---
+++ loading.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/loading.css">
+  <title>積込状況管理</title>
+</head>
+<body>
+
+  <nav class="header">
+    <a href="index.php" class="home">HOME</a>
+  </nav>
+
+  
+  <div class="container">
+    <div>
+      <div class="firsttable">
+        <table>
+          <tr>
+            <th>タイトル</th>
+          </tr>
+          <tr><td>aaaaaaa</td></tr>
+          <tr><td>aaaaaaa</td></tr>
+          <tr><td>aaaaaaa</td></tr>
+          <tr><td>aaaaaaa</td></tr>
+          <tr><td>aaaaaaa</td></tr>
+          <tr><td>aaaaaaa</td></tr>
+          <tr><td>aaaaaaa</td></tr>
+          <tr><td>aaaaaaa</td></tr>
+          <tr>
+            <td id="pagenumber">1/1ページ</td>
+          </tr>
+        </table>
+      </div>
+
+      <div class="secondtable">
+        <table>
+            <tr>
+                <th>タイトル１</th>
+                <th>タイトル２</th>
+                <th>タイトル３</th>
+                <th></th>
+            </tr>
+            <tr>
+                <td id="kisu">1/1</td>
+                <td id="kisu">2/1</td>
+                <td id="kisu">3/1</td>
+                <td id="kisu">しばらくお待ちください。</td>
+            </tr>
+            <tr>
+                <td>1/2</td>
+                <td>2/2</td>
+                <td>3/2</td>
+                <td>しばらくお待ちください。</td>
+            </tr>
+            </tr>
+            <tr>
+                <td id="kisu">1/3</td>
+                <td id="kisu">2/3</td>
+                <td id="kisu">3/3</td>
+                <td id="kisu">しばらくお待ちください。</td>
+            </tr>
+            <tr>
+                <td>1/4</td>
+                <td>2/4</td>
+                <td>3/4</td>
+                <td>しばらくお待ちください。</td>
+            </tr>
+            </tr>
+            <tr>
+                <td id="kisu">1/5</td>
+                <td id="kisu">2/5</td>
+                <td id="kisu">3/5</td>
+                <td id="kisu">しばらくお待ちください。</td>
+            </tr>
+            <tr>
+                <td>1/6</td>
+                <td>2/6</td>
+                <td>3/6</td>
+                <td>しばらくお待ちください。</td>
+            </tr>
+            <tr>
+                <td id="kisu">1/6</td>
+                <td id="kisu">2/6</td>
+                <td id="kisu">3/6</td>
+                <td id="kisu">しばらくお待ちください。</td>
+            </tr>
+            <tr>
+                <td>1/6</td>
+                <td>2/6</td>
+                <td>3/6</td>
+                <td>しばらくお待ちください。</td>
+            </tr>
+            <tr>
+                <td id="kisu">1/6</td>
+                <td id="kisu">2/6</td>
+                <td id="kisu">3/6</td>
+                <td id="kisu">しばらくお待ちください。</td>
+            </tr>
+            <tr>
+            <td colspan="4" id="pagenumber">1/1ページ</td>
+            </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+
+</body>
+</html>
